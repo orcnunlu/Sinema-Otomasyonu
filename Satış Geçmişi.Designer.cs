b4e97b57ea63445c96e1d596183b141a4feb64a3
Orçun Ünlü <118480028+orcnunlu@users.noreply.github.com>
2023-02-02 16:55:36 +0300

    Add files via upload

---
+++ Satış Geçmişi.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Sinema_Otomasyonu
+{
+    partial class Satış_Geçmişi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Satış_Geçmişi));
+            this.SuspendLayout();
+            // 
+            // Satış_Geçmişi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sinema_Otomasyonu.Properties.Resources._14773;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Satış_Geçmişi";
+            this.Text = "Satış Geçmişi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
